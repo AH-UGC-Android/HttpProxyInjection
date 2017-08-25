73350b32fd3092e10f0eff1c958f27a2a6b04cd8
赵春雨 <zhaochunyu@autohome.com.cn>
2017-08-25 18:34:16 +0800

    小修改

--- app/src/main/java/tn/ln/httpproxyinjection/InjectionInterceptor.java
+++ app/src/main/java/tn/ln/httpproxyinjection/InjectionInterceptor.java
@@ -1,22 +1,45 @@
 package tn.ln.httpproxyinjection;
 
+import android.util.Log;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.Buffer;
 
 /**
  * Created by zhchyu999 on 2017/8/19.
  */
 
 public class InjectionInterceptor implements Interceptor {
+    public static final String TAG="InjectionInterceptor";
     @Override
     public Response intercept(Chain chain) throws IOException {
 
         Request request=chain.request();
-        request.body().
+        Log.d(TAG,request.url().encodedPath());
+//        request.body().
         Response response= chain.proceed(request);
+//        Log.d(TAG,response.body().string());
         return response;
+    }
+
+
+    public static String bodyToString(final RequestBody request){
+        try {
+            final RequestBody copy = request;
+            final Buffer buffer = new Buffer();
+            if(copy != null)
+                copy.writeTo(buffer);
+            else
+                return "";
+            return buffer.readUtf8();
+        }
+        catch (final IOException e) {
+            return "did not work";
+        }
     }
 }
